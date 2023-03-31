ca27f4c4789a8a927d6464ed8e3c9aa4e5fabe3a
Saul Ceja <saulceja08@gmail.com>
2023-03-31 15:56:58 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+//Create an equation to multiply two nums
+let num1,num2,multiply;
+
+num1 = 31;
+num2 = 2;
+multiply = num1*num2;
 
 // Write your code here
+let random = Math.ceil(Math.random()*10);
+
+//Write your code here
+let num3,num4,mod;
+num3 = 34;
+num4 = 5;
+mod = num3 % num4;
+
+//Write your code here
+let max;
+max = Math.max(1,2,3,20);
